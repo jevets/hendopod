acf6b984f4940a50d770bebee42f064767b2c99b
stevejamesson <steve@evendev.com>
2018-10-23 18:07:30 -0400

    purgecss now working w/ extractor custom

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 const gulp = require('gulp')
+const run = require('gulp-run-command').default
 
-gulp.task('css:compile', () => {
+/**
+ * HTML Compilation & Cleanup
+ */
+gulp.task('html:compile', run('eleventy'))
+
+gulp.task('html', ['html:compile'])
+
+/**
+ * CSS Processing & Cleanup
+ */
+gulp.task('css:compile', ['html:compile'], () => {
   return gulp.src('src/styles.css')
     .pipe(require('gulp-postcss')([
       require('tailwindcss')('./tailwind.js'),
@@ -9,4 +20,27 @@ gulp.task('css:compile', () => {
     .pipe(gulp.dest('dist'))
 })
 
-gulp.task('css', ['css:compile'])
+gulp.task('css:purge', ['html:compile', 'css:compile'], () => {
+  return gulp.src('dist/styles.css')
+    .pipe(require('gulp-purgecss')({
+      content: ['dist/index.html'],
+      extractors: [
+        {
+          extractor: class TailwindExtractor {
+            static extract(content) {
+              return content.match(/[A-z0-9-:\/]+/g) || [];
+            }
+          },
+          extensions: ['css', 'html'],
+        }
+      ]
+    }))
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('css', ['css:compile', 'css:purge'])
+
+/**
+ * Default (all tasks)
+ */
+gulp.task('default', ['html', 'css'])
